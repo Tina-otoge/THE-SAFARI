d3f6c8ab88252c4c92b271cff62515152f3f6080
SKiEL <skielred@gmail.com>
2016-09-24 01:28:20 +0200

    Basic security variables and post functions

--- config.php
+++ config.php
@@ -5,9 +5,24 @@ $db_name = 'safari';
 $db_user = 'root';
 $db_pwd = '';
 
-// SOMETHING ELSE
-/* code
- * code
- * code
- * code
+// PASSWORDS SECURITY
+// Warning : This session is very commented.
+/* You should leave this to true. It generates a random hash salt and stores it
+ * in a physical file, this way, every THE SAFARI system will have a different
+ * and unique salt, which isn't stealable from the database.
+ * (Of course this can be used alongside other security methods)
  */
+$hash_generate_salt_file = true;
+/* Useful if you doesn't trust my algorithm and want to use your own
+ * (This won't create a file, it will use the existent one in /hash_salt)
+ */
+$hash_use_salt_file = true;
+/* This salt will be mixed in the hashing of the users password.
+ * This isn't more efficient than the generated salt file, but it's useful if
+ * you want a little more or doesn't want to a salt file at all.
+ * ***
+ * Don't hesitate to put something weird, long, short, special characters,
+ * basically anything. But please, don't leave it default.
+ * Leave an empty string '' if you do not want to use the bonus salt.
+ */
+$hash_bonus_salt = 'Following @skielred on Twitter changed my life';
--- core.php
+++ core.php
@@ -3,17 +3,37 @@ require_once 'config.php';
 
 // At this point, all the parameters to initiate a mysqli connection should have been declared
 // This is preferably done in the config.php file
+
 $db = new mysqli($db_host, $db_user, $db_pwd, $db_name);
 if ($db->connect_errno) {
  // header('Location: 500');
 }
 
-function array_posts ($limit, $offset=0, $query=false) {
+// POSTS
+/* array_posts ( (int) limit [, (int) offset, (string) query]) => (array)
+ * Fetch $limit posts from database starting from 0 or $offset using a generic
+ * or given $query and return them into an array.
+ */
+function array_posts ($limit, $offset=0, $query='') {
   global $db;
-  $query = $query ? $query : 'SELECT id, name, url, author_id, contributors_id, posting_time, updating_time, synopsis, has_preview, tags_id FROM posts';
+  $query = !empty($query) ? $query : 'SELECT id, name, url, author_id, contributors_id, posting_time, updating_time, synopsis, has_preview, tags_id FROM posts';
   $query .= ' LIMIT '.$offset.','.$limit;
   $res = $db->query($query);
   $row = $res->fetch_all(MYSQLI_ASSOC);
   $res->free();
   return $row;
+}
+/* get_post ( (any) item [, (string) url]) => (array)
+ * Fetch a post with $item as a value for $col (url if nothing is passed as a
+ * second argument) and return every data from it in an array.
+ */
+function get_post ($item, $col='url') {
+  global $db;
+  if ($stmt = $db->prepare('SELECT * FROM posts WHERE '.$col.' = ?')) {
+    $stmt->bind_param('s',$item);
+    $stmt->execute();
+    $res = $stmt->get_result(); $stmt->close();
+    return $res->num_rows === 0 ? false : $res->fetch_assoc();
+  }
+  
 }
