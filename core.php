23491abe17caa962cedfa95f25cf7c765b013f5a
SKiEL <skielred@gmail.com>
2016-09-24 03:53:18 +0200

    Ghetto write edit and delete

--- core.php
+++ core.php
@@ -12,7 +12,7 @@ if ($db->connect_errno) {
 // POSTS
 /* array_posts ( (int) limit [, (int) offset, (string) query]) => (array)
  * Fetch $limit posts from database starting from 0 or $offset using a generic
- * or given $query and return them into an array.
+ * or given $query and return them into an array
  */
 function array_posts ($limit, $offset=0, $query='') {
   global $db;
@@ -25,7 +25,7 @@ function array_posts ($limit, $offset=0, $query='') {
 }
 /* get_post ( (any) item [, (string) url]) => (array)
  * Fetch a post with $item as a value for $col (url if nothing is passed as a
- * second argument) and return every data from it in an array.
+ * second argument) and return every data from it in an array
  */
 function get_post ($item, $col='url') {
   global $db;
@@ -35,5 +35,44 @@ function get_post ($item, $col='url') {
     $res = $stmt->get_result(); $stmt->close();
     return $res->num_rows === 0 ? false : $res->fetch_assoc();
   }
-  
+}
+/* write_post ( (string) name, (string) url, (int) author_id, (string) contributors_id, (string) content, (string) synopsis, (int) has_preview, (string) tags_id) => (void)
+ * Call the function with correct arguments to append a blog post to the posts table in the database
+ */
+function write_post ($name, $url, $author_id, $contributors_id, $content, $synopsis, $has_preview, $tags_id) {
+  global $db;
+  if ($stmt = $db->prepare('INSERT INTO posts (name, url, author_id, contributors_id, posting_time, content, synopsis, has_preview, tags_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)')) {
+    $time = date('Y-m-d h:i:s');
+    $url = empty($url) ? construct_url($name) : $url;
+    $stmt->bind_param('ssissssis', $name, $url, $author_id, $contributors_id, $time, $content, $synopsis, $has_preview, $tags_id);
+    $stmt->execute(); $stmt->close();
+  }
+}
+/* update_post ( (array) array, (any) item [, (string) url]) => (void) */
+function update_post ($array, $item, $col='url') {
+  global $db;
+  $prev = get_post($item,$col);
+  $new = array_replace($prev,(array_intersect_key($array,$prev)));
+  $query = 'UPDATE posts SET ';
+  foreach ($new as $key => $value) {
+    if (is_int($value)) {
+    $query .= '`'.$key.'` = '.$value.', ';
+    } else {
+    $query .= '`'.$key.'` = \''.$value.'\', ';
+    }
+  }
+  $query = substr($query,0,-2).' WHERE `posts`.`id` = '.$new['id'].'';
+  return $db->query($query);
+}
+/* delete_post ( (any) item [, (string) url]) => (array) */
+function delete_post($item, $col='url') {
+  global $db;
+  $stmt = $db->prepare('DELETE FROM `posts` WHERE `posts`.`'.$col.'` = ?');
+  $stmt->bind_param('s',$item);
+  $stmt->execute(); $stmt->close();
+}
+// TODO
+function construct_url($string) {
+  // do something
+  return $string;
 }
